890eabb9c79c2413f6daca5b887b1657777341da
vanessa gertman <vanessagertman@gmail.com>
2020-04-04 15:22:33 -0600

    Start implementing website redesign

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,15 @@
 module.exports = {
     plugins: [],
-    theme: {extend: {}},
+    theme: {
+        extend: {
+            colors: {
+                'ac-blue': '#2A5BD7',
+                'ac-dark-blue': '#173276',
+                'ac-dark-gray': '#3F434E',
+                'ac-light-gray': '#E5E5E5',
+                'ac-light-blue': '#CAD6F5'
+            }
+        }
+    },
     variants: {}
 }
